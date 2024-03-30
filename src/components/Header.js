4e7790016d2a325bd531f6d8fe8837a98038ebcf
Deniansyah <deniansyah814@gmail.com>
2024-03-30 15:22:10 +0700

    update components header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,10 @@ import React from "react";
 const Header = (props) => {
   return (
     <div className="bg-[#5E50A1] mb-[50xp] pt-[100px]">
-      <div className="px-5 md:px-[150px] py-[33px]">
-        <span className="text-[28px] leading-[20px] text-white font-bold">Top Jobs</span>
+      <div className="flex justify-start items-center gap-5 px-4 md:px-[150px] py-5">
+        <p className="text-3xl text-white font-bold">Top Jobs</p>
+        <p className="text-3xl text-white">-</p>
+        <p className="text-lg text-white">Lengkapi informasi pribadi anda di profile agar terpublikasi !</p>
       </div>
     </div>
   );
